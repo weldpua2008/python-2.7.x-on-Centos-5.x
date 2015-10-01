93939c4345f915241e0ca17a00233af1ad8f8cad
Valery Solovyov <weldpua2008@gmail.com>
2015-10-02 01:57:10 +0400

    try to fix /python27/testing/test.sh: line 13: git: command not found

--- testing/test.sh
+++ testing/test.sh
@@ -9,8 +9,12 @@ BATS_TEST="${RDIR}/tests"
 ######################################
 # Bats tests: https://github.com/sstephenson/bats
 ######################################
-yum install -y git
-git clone https://github.com/sstephenson/bats.git && cd bats && ./install.sh /usr/local || (echo "Bats wasn't installed";exit 1)
+rpm --import http://apt.sw.be/RPM-GPG-KEY.dag.txt
+rpm -i http://packages.sw.be/rpmforge-release/rpmforge-release-0.5.2-2.el5.rf.`uname -i`.rpm
+#yum update
+yum install git -y
+yum install git-gui -y
+git clone https://github.com/sstephenson/bats.git && cd bats && ./install.sh /usr/local || (echo "Bats wasn't installed"; exit 1)
 
 set -e
 bats --tap ${BATS_TEST}
